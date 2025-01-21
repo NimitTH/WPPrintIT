54e88789aae2a8bb81e519161ee0244e21dade62
nimit <nimitzth@gmail.com>
2025-01-21 10:10:48 +0700

    Your commit message here

--- src/app/api/user/status/[id]/route.ts
+++ src/app/api/user/status/[id]/route.ts
@@ -1,9 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 
-export async function PUT(request: NextRequest, { params }: { params: { id: string } }) {
+export async function PUT(req: NextRequest, { params }: { params: { id: string } }) {
     try {
-        const { status } = await request.json();
+        const { status } = await req.json();
         const { id } = params;
 
         await prisma.user.update({
